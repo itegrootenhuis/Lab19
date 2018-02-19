84584bcf48995c6bffa1ef61bfe2b65dbb444ab0
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-02-19 17:53:06 -0500

    Got vue working for products except for images

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var products = [];
+
+    function init(){
+        // GetDataFromJsonFile();
+    }
+
+    // function GetDataFromJsonFile(){
+    //     $.getJson("products.json", function(data){
+
+    //     });
+    // }
+
+    init();
+});
